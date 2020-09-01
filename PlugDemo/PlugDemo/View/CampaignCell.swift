dca2017e3db67f428959899c2f301cca81c59e16
pitt500 <pedro.rojaslo50@gmail.com>
2020-08-31 23:09:34 -0700

    Configure base layout to match design

--- PlugDemo/PlugDemo/View/CampaignCell.swift
+++ PlugDemo/PlugDemo/View/CampaignCell.swift
@@ -5,4 +5,78 @@
 //  Created by projas on 8/31/20.
 //
 
-import Foundation
+import UIKit
+
+class CampaignCell: UICollectionViewCell {
+  private var vStackView: UIStackView = {
+    let stack = UIStackView()
+    stack.axis = .vertical
+    stack.spacing = 4
+    stack.translatesAutoresizingMaskIntoConstraints = false
+    return stack
+  }()
+  
+  private var hStackView: UIStackView = {
+    let stack = UIStackView()
+    stack.axis = .horizontal
+    stack.distribution = .fillEqually
+    stack.spacing = 4
+    stack.translatesAutoresizingMaskIntoConstraints = false
+    return stack
+  }()
+  
+  var mediaView: UIView = {
+    let view = UIView()
+    view.backgroundColor = .red
+    view.translatesAutoresizingMaskIntoConstraints = false
+    return view
+  }()
+  
+  var trackingLinkView: UIView {
+    let view = UIView()
+    view.backgroundColor = .green
+    view.translatesAutoresizingMaskIntoConstraints = false
+    return view
+  }
+  
+  var downloadView: UIView {
+    let view = UIView()
+    view.backgroundColor = .blue
+    view.translatesAutoresizingMaskIntoConstraints = false
+    return view
+  }
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setConstraints()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+}
+
+extension CampaignCell {
+  func setConstraints() {
+    addSubview(vStackView)
+    vStackView.addArrangedSubview(mediaView)
+    vStackView.addArrangedSubview(hStackView)
+    hStackView.addArrangedSubview(trackingLinkView)
+    hStackView.addArrangedSubview(downloadView)
+    
+    NSLayoutConstraint.activate([
+      topAnchor.constraint(equalTo: vStackView.topAnchor),
+      bottomAnchor.constraint(equalTo: vStackView.bottomAnchor),
+      leadingAnchor.constraint(equalTo: vStackView.leadingAnchor),
+      trailingAnchor.constraint(equalTo: vStackView.trailingAnchor),
+      
+      vStackView.bottomAnchor.constraint(equalTo: hStackView.bottomAnchor),
+      hStackView.heightAnchor.constraint(equalToConstant: 50),
+      
+      vStackView.topAnchor.constraint(equalTo: mediaView.topAnchor),
+      vStackView.leadingAnchor.constraint(equalTo: mediaView.leadingAnchor),
+      vStackView.trailingAnchor.constraint(equalTo: mediaView.trailingAnchor),
+    ])
+  }
+}
--- PlugDemo/PlugDemo/View/ViewController.swift
+++ PlugDemo/PlugDemo/View/ViewController.swift
@@ -11,20 +11,66 @@ class CampaignViewController: UIViewController {
   
   let service: CampaignOperation = CampaignService()
   
+  let collectionView: UICollectionView = {
+    let layout = UICollectionViewFlowLayout()
+    let padding: CGFloat = 4
+    layout.sectionInset = UIEdgeInsets(top: padding, left: padding , bottom: padding, right: padding)
+    layout.itemSize = CGSize(width: 150, height: 250)
+    layout.scrollDirection = .horizontal
+    
+    let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+    collection.backgroundColor = .brown
+    return collection
+  }()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    view.backgroundColor = .green
+    view.backgroundColor = .white
     
-    service.getFeed { result in
-      switch result {
-      case .success(let campaigns):
-        print(campaigns)
-      case .failure(let error):
-        print(error)
-      }
-    }
+//    service.getFeed { result in
+//      switch result {
+//      case .success(let campaigns):
+//        print(campaigns)
+//      case .failure(let error):
+//        print(error)
+//      }
+//    }
+    setConstraints()
+    configureCollectionView()
   }
 
+}
 
+extension CampaignViewController {
+  func setConstraints() {
+    view.addSubview(collectionView)
+    
+    collectionView.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      self.view.safeAreaLayoutGuide.topAnchor.constraint(equalTo: collectionView.topAnchor),
+      self.view.safeAreaLayoutGuide.leadingAnchor.constraint(equalTo: collectionView.leadingAnchor),
+      self.view.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: collectionView.trailingAnchor),
+      
+      collectionView.heightAnchor.constraint(equalToConstant: 260)
+    ])
+  }
+  
+  func configureCollectionView() {
+    collectionView.dataSource = self
+    collectionView.register(CampaignCell.self, forCellWithReuseIdentifier: "cell")
+  }
+}
+
+extension CampaignViewController: UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 5
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CampaignCell
+    return cell
+  }
+  
+  
 }
 
