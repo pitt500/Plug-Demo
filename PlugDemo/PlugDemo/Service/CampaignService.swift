c84ff7c583ad279ccf722830b0de652f01b85e2e
pitt500 <pedro.rojaslo50@gmail.com>
2020-08-31 21:27:47 -0700

    Configure feed method to retrieve campaign list from API

--- PlugDemo/PlugDemo/Service/CampaignService.swift
+++ PlugDemo/PlugDemo/Service/CampaignService.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+class CampaignService: CampaignOperation {
+  let session = URLSession.shared
+  
+  func getFeed(completion: @escaping ([Campaign]) -> Void, failure: @escaping (Error) -> Void) {
+    let url = URL(string: "https://www.plugco.in/public/take_home_sample_feed")
+    let request = URLRequest(url: url!)
+    session.dataTask(with: request) { (data, response, error) in
+      
+      if let error = error {
+        failure(error)
+        return
+      }
+      
+      guard let data = data else {
+        failure(NetworkError.noData)
+        return
+      }
+      
+      let decoder = JSONDecoder()
+      decoder.keyDecodingStrategy = .convertFromSnakeCase
+      
+      guard let decoded = try? decoder.decode(CampaignResponse.self, from: data) else {
+        failure(NetworkError.invalidFormat)
+        return
+      }
+      
+      completion(decoded.campaigns)
+      
+    }.resume()
+  }
+  
+}
