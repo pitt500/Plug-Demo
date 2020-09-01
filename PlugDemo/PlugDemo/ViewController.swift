199e1833e936ac86d4dbfcfd7ab08452aa4a935f
pitt500 <pedro.rojaslo50@gmail.com>
2020-08-31 20:50:17 -0700

    Add starter project

---
+++ PlugDemo/PlugDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlugDemo
+//
+//  Created by projas on 8/31/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
