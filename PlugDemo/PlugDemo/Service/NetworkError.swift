3bddd68b6699d8d2d7989c8be86e06e07c9e3342
pitt500 <pedro.rojaslo50@gmail.com>
2020-08-31 21:26:55 -0700

    Add enum with network errors

---
+++ PlugDemo/PlugDemo/Service/NetworkError.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkError.swift
+//  PlugDemo
+//
+//  Created by projas on 8/31/20.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+  case defaultError
+  case noData
+  case invalidFormat
+}
