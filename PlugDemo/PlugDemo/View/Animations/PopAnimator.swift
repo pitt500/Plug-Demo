c2197379b807c351d2f5a5c192bb59ebdb553317
pitt500 <pedro.rojaslo50@gmail.com>
2020-09-06 00:48:24 -0700

    Add PopAnimator

---
+++ PlugDemo/PlugDemo/View/Animations/PopAnimator.swift
@@ -0,0 +1,18 @@
+//
+//  PopAnimator.swift
+//  PlugDemo
+//
+//  Created by projas on 9/6/20.
+//
+
+import UIKit
+
+class PopAnimator: NSObject, UIViewControllerAnimatedTransitioning {
+  func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+    return 0
+  }
+  
+  func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+    
+  }
+}
