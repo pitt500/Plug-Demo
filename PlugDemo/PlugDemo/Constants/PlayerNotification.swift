30abdf180bf2906529e8eaf7daa7460e406de261
pitt500 <pedro.rojaslo50@gmail.com>
2020-09-07 17:05:05 -0700

    Add Custom notifications for player controls

---
+++ PlugDemo/PlugDemo/Constants/PlayerNotification.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerNotification.swift
+//  PlugDemo
+//
+//  Created by projas on 9/7/20.
+//
+
+import Foundation
+
+enum PlayerNotification {
+  static let dismissAfterTime = Notification.Name("dismissAfterTime")
+  static let invalidateTimer = Notification.Name("invalidateTimer")
+}
